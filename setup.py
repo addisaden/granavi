463026f426a18dc32f186bdf0954e6ead778bc26
Adrian Enns <addis.aden@gmail.com>
2022-12-30 09:15:59 +0100

    basic structure

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='granavi',
+      version='0.1',
+      description='Graph navigation',
+      url='http://github.com/addisaden/granavi',
+      author='Adrian E.',
+      author_email='',
+      license='MIT',
+      packages=['granavi'],
+      zip_safe=False)
