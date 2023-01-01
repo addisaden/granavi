5bd959aeb1b71587209c62df7638428c1debd527
Adrian Enns <addis.aden@gmail.com>
2023-01-01 11:15:31 +0100

    search a path and representation with name

--- granavi/node.py
+++ granavi/node.py
@@ -14,3 +14,31 @@ class Node:
 
     def connectedNodes(self):
         return self.__connectedNodes__
+
+    def pathTo(self, other):
+        visited = []
+        currentPath = [self]
+        while currentPath[-1] != other:
+            currentNode = currentPath[-1]
+            if currentNode not in visited:
+                visited.append(currentNode)
+            foundNeighbor = None
+            for neighbor in currentNode.connectedNodes():
+                if neighbor in visited:
+                    continue
+                if foundNeighbor is None or neighbor is other:
+                    foundNeighbor = neighbor
+            if foundNeighbor is None:
+                if currentNode is self:
+                    raise Exception("Not connected")
+                else:
+                    currentPath.pop()
+            else:
+                currentPath.append(foundNeighbor)
+        
+        return currentPath
+
+    def __repr__(self):
+        classname = ".".join([self.__class__.__module__, self.__class__.__name__])
+        selfid = hex(id(self))
+        return "<{} object {} at {}>".format(classname, self.name, selfid)
--- tests/test_node.py
+++ tests/test_node.py
@@ -24,4 +24,20 @@ class NodeTest(unittest.TestCase):
                 node_1.connect(wrongNode)
         self.assertTrue(node_1.isConnected(node_2))
         self.assertTrue(node_2 in node_1.connectedNodes())
-        
+
+    def test_route_simple(self):
+        node_1 = granavi.Node("1")
+        node_2 = granavi.Node("2")
+        node_3 = granavi.Node("3")
+        node_1.connect(node_2)
+        node_2.connect(node_3)
+        self.assertEqual(node_1.pathTo(node_3), [node_1, node_2, node_3])
+        node_1.connect(node_3)
+        self.assertEqual(node_1.pathTo(node_3), [node_1, node_3])
+
+    def test_repr_string(self):
+        node_1 = granavi.Node("nodeName")
+        node_1_repr = node_1.__repr__()
+        self.assertTrue(node_1_repr.find("nodeName") > -1)
+        self.assertTrue(node_1_repr.find(hex(id(node_1))) > -1)
+        self.assertTrue(node_1_repr.find(".".join([node_1.__class__.__module__, node_1.__class__.__name__])) > -1)
