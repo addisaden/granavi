0142d519c3299b5260ae6f2560e37ee2613cc220
Adrian Enns <addis.aden@gmail.com>
2023-01-03 22:34:50 +0100

    quicksave

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from setuptools import setup
 
 setup(name='granavi',
-      version='0.1',
+      version='0.0.1',
       description='Graph navigation',
       url='http://github.com/addisaden/granavi',
       author='Adrian E.',
       author_email='',
       license='MIT',
       packages=['granavi'],
-      zip_safe=False)
+      zip_safe=False,
+      install_requires=[])
